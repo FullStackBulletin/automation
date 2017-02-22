772db5e1648701afcd45d3e56506f4d275de609c
Luciano Mammino <luciano@planet9energy.com>
2017-02-22 10:15:16 +0000

    Improving mailchimp integration

--- src/mailchimpCampaign.js
+++ src/mailchimpCampaign.js
@@ -1,3 +1,14 @@
+const escapeAttrNodeValue = value =>
+  value.replace(/(&)|(")|(\u00A0)/g, (match, amp, quote) => {
+    if (amp) return '&amp;';
+    if (quote) return '&quot;';
+    return '&nbsp;';
+  })
+;
+
+const createImageHtml = (url, title) =>
+  `<img alt="${escapeAttrNodeValue(title)}" src="${url}" width="194" style="max-width:500px;" class="mcnImage">`;
+
 export const createCampaignFactory = (httpClient, apiKey) => {
   const [, dc] = apiKey.split('-');
   const apiEndpoint = `https://user:${apiKey}@${dc}.api.mailchimp.com/3.0`;
@@ -9,10 +20,10 @@ export const createCampaignFactory = (httpClient, apiKey) => {
         list_id: campaignSettings.listId,
       },
       settings: {
-        subject_line: 'Some subject',
-        title: 'Some title',
-        from_name: 'from_name',
-        reply_to: 'some_reply_to',
+        subject_line: 'Some subject', // TODO
+        title: 'Some title',  // TODO
+        from_name: 'from_name@fullstackbulletin.com',  // TODO
+        reply_to: 'some_reply_to',  // TODO
       },
     };
 
@@ -24,6 +35,7 @@ export const createCampaignFactory = (httpClient, apiKey) => {
         template: {
           id: campaignSettings.templateId,
           sections: {
+            content_preview: 'some preview', // TODO
             quote_text: quote.text,
             quote_author: quote.author,
             quote_author_description: quote.authorDescription,
@@ -35,6 +47,7 @@ export const createCampaignFactory = (httpClient, apiKey) => {
       links.forEach((link, i) => {
         contentData.template.sections[`article_title_${i + 1}`] = link.title;
         contentData.template.sections[`article_description_${i + 1}`] = link.description;
+        contentData.template.sections[`image_${i + 1}`] = createImageHtml(link.image, link.title);
       });
 
       return httpClient.put(createCampaignContentUrl, contentData);
