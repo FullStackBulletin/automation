9d2fea81381741888c7478816fce3393198bca42
Luciano Mammino <luciano@planet9energy.com>
2017-02-22 14:37:10 +0000

    Improving cloudinary uploader lib

--- src/uploadImagesToCloudinary.js
+++ src/uploadImagesToCloudinary.js
@@ -1,6 +1,13 @@
 import { mapLimit } from 'async';
+import { createHash } from 'crypto';
 
-const uploadImageToCloudinary = cloudinaryClient => (urlInfo, cb) => {
+const uploadImageToCloudinary = (cloudinaryClient, folder) => (urlInfo, cb) => {
+  const publicId = `${folder}/${createHash('md5').update(urlInfo.image).digest('hex')}`;
+
+  // TODO check if the file was already uploaded with:
+  // cloudinary.api.resource('sample',
+  // function(result)  { console.log(result) });
+
   cloudinaryClient.uploader.upload(urlInfo.image, (uploadedInfo) => {
     if (uploadedInfo.error) {
       return cb(uploadedInfo.error);
@@ -17,13 +24,13 @@ const uploadImageToCloudinary = cloudinaryClient => (urlInfo, cb) => {
     const image = cloudinaryClient.url(uploadedInfo.public_id, transformations);
 
     return cb(null, { ...urlInfo, image, originalImage: urlInfo.image });
-  });
+  }, { public_id: publicId });
 };
 
-export const uploadImagesToCloudinary = cloudinaryClient => urlsInfo =>
+export const uploadImagesToCloudinary = (cloudinaryClient, folder) => urlsInfo =>
   new Promise((resolve, reject) => {
     const limit = 10;
-    const upload = uploadImageToCloudinary(cloudinaryClient);
+    const upload = uploadImageToCloudinary(cloudinaryClient, folder);
     mapLimit(urlsInfo, limit, upload, (err, urlsInfoWithUploadLinks) => {
       if (err) {
         return reject(err);
